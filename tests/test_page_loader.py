db2da8e9f9018608dbce79bdeb3b09ee3e2a371d
Sergey Martynov <acemore@mail.ru>
2022-02-02 13:44:55 +0300

    Update tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -2,21 +2,28 @@ import os
 import pytest
 import requests_mock
 import tempfile
+
 from page_loader import download
 from page_loader.page_loader import get_loaded_file_full_path
+from page_loader.hyphenated_str_getter import get_hyphenated_name
+from page_loader.loaded_html_page_modifier import FILES_POSTFIX
 
 
+EXPECTED_HTML_PATH = 'tests/fixtures/expected.html'
 EXPECTED_LOADED_FILE_NAME = 'ru-hexlet-io-courses.html'
-FIRST_URL_WITH_HTTP = 'http://ru.hexlet.io/courses'
-FIRST_URL_WITH_HTTPS = 'https://ru.hexlet.io/courses/'
-SECOND_URL = 'https://page-loader.hexlet.repl.co'
+EXPECTED_LOADED_IMAGE_NAME = 'page-loader-hexlet-repl-co-assets-professions-nodejs.png'
+ORIGINAL_HTML_PATH = 'tests/fixtures/original.html'
+SITE_IMAGE_URL = 'https://page-loader.hexlet.repl.co/assets/professions/nodejs.png'
+SITE_MAIN_PAGE_URL = 'https://page-loader.hexlet.repl.co'
+URL_WITH_HTTP = 'http://ru.hexlet.io/courses'
+URL_WITH_HTTPS = 'https://ru.hexlet.io/courses/'
 
 
 @pytest.mark.parametrize(
     'url',
     [
-        FIRST_URL_WITH_HTTPS,
-        FIRST_URL_WITH_HTTP
+        URL_WITH_HTTPS,
+        URL_WITH_HTTP
     ]
 )
 def test_loaded_file_full_path(url):
@@ -26,15 +33,30 @@ def test_loaded_file_full_path(url):
         )
 
 
-def test_loaded_file_content():
-    with open('tests/fixtures/expected.html') as expected_html:
-        expected_loaded_file_content = expected_html.read()
+def test_loaded_files():
+    with open(ORIGINAL_HTML_PATH) as original_html:
+        original_html_content = original_html.read()
+
+    with open(EXPECTED_HTML_PATH) as expected_html:
+        expected_loaded_page_content = expected_html.read()
 
     with requests_mock.Mocker() as m:
-        m.get(SECOND_URL, text=expected_loaded_file_content)
+        m.get(SITE_MAIN_PAGE_URL, text=original_html_content)
+        m.get(SITE_IMAGE_URL)
 
         with tempfile.TemporaryDirectory() as temp_dir:
-            loaded_file_path = download(SECOND_URL, temp_dir)
+            loaded_page_path = download(SITE_MAIN_PAGE_URL, temp_dir)
 
-            with open(loaded_file_path) as loaded_file:
-                assert loaded_file.read() == expected_loaded_file_content
+            local_resources_dir_name = (
+                get_hyphenated_name(SITE_MAIN_PAGE_URL, FILES_POSTFIX)
+            )
+            local_resources_dir_path = os.path.join(
+                temp_dir,
+                local_resources_dir_name
+            )
+            assert EXPECTED_LOADED_IMAGE_NAME in (
+                os.listdir(local_resources_dir_path)
+            )
+
+            with open(loaded_page_path) as loaded_page:
+                assert loaded_page.read() == expected_loaded_page_content
